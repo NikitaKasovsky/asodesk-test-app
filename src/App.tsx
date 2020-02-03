f350c039e82c3a3cf7ea4365a61cdf8d537ea6a0
NikitaKasovsky <nikita@kasovsky.ru>
2020-02-03 22:26:56 +0300

    create base markup page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
+
+// Стили
 import './App.css';
+
+// Компоненты
+import Table from './table/Table';
 
 const App = () => {
   return (
-    <div>
-      <h3>is it work's!</h3>
+    <div className="wrapper">
+      <div className="header"></div>
+      <div className="side-nav"></div>
+      <div className="content">
+        <Table />
+      </div>
     </div>
   );
 }
---
+++ src/table/Table.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// Стили
+import './table.css';
+
+export default function Table() {
+  return (
+    <div className="wrapper-table">
+      <div className="content">1</div>
+    </div>
+  )
+}
